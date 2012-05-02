fb795b1a947290be3cbdf69859e44baef1232a2d
Gennady Borukhovich <gboru@efficientclutch.com>
2012-05-02 15:08:15 -0400

    responses

--- homework_1/ViewController.m
+++ homework_1/ViewController.m
@@ -28,9 +28,9 @@
     imageView.animationRepeatCount = 0;//repeat forever.
     [imageView startAnimating];
     [self.view addSubview:imageView];//play with some animation feature.
-   scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(100, 100, 580, 800)];
+    scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(100, 100, 580, 800)];
     scrollView.backgroundColor = [UIColor grayColor];
-    scrollView.alwaysBounceHorizontal = YES;//I can't tell there is any difference between "YES" and "NO".
+    scrollView.alwaysBounceHorizontal = NO;//I can't tell there is any difference between "YES" and "NO".
     [scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
     scrollView.contentSize = CGSizeMake(2048, 1536);
     [scrollView addSubview:imageView];
@@ -40,10 +40,11 @@
     	// Do any additional setup after loading the view, typically from a nib.
 }
 
--(IBAction)didPressAnimate:(id)sender{//what's the difference between (ibaction)(id)sender and (void)?
-    UIImageView *animation = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"cake.jpg"]];//can't use pdf file.
+-(IBAction)didPressAnimate:(id)sender{//what's the difference between (ibaction)(id)sender and (void)? 
+//GB: IBaction is for when you want to tell Interface Builder about this method
+    UIImageView *animation = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"cake.jpg"]];//can't use pdf file. GB:pdf is not considered an image
     animation.frame = CGRectMake(10, 10, 338, 450);
-    [self.view addSubview:animation];
+    [self.view addSubview:animation];//GB: Why do you add it to both? 
     [scrollView addSubview:animation];
     [self moveImage:animation duration:20.0 curve:UIViewAnimationCurveEaseInOut x:1710.0 y:1086.0];//there are easein or out or line feature.
      //NSLog(@"yeah~~~~");
